c3cf163bb9d4b4e8feb13da3718a440cbe7c8d44
kangkang <1522847010@qq.com>
2016-08-06 18:34:18 +0800

    查询操作

--- Test_Python_MySql/HelloWord.py
+++ Test_Python_MySql/HelloWord.py
@@ -6,6 +6,7 @@ Created on 2016年8月6日
 测试Python使用MySQL
 '''
 import MySQLdb
+from sqlalchemy.sql import sqltypes
 
 print MySQLdb
 
@@ -25,6 +26,23 @@ cursor = conn.cursor()
 #3.执行sql语句
 print conn
 print cursor
+
+#使用cursor.execute()执行select语句
+sql = "select * from user"
+cursor.execute(sql)
+print cursor.rowcount#获取结果集个数
+
+rs = cursor.fetchone()#获取当前第一条数据
+print rs
+
+
+rs = cursor.fetchmany(3)#获取多条数据
+print rs
+
+rs = cursor.fetchall()#获取剩余的数据
+print rs
+
+#使用cursor.fetch*()获取并处理数据
 
 #4.关闭资源
 cursor.close()
