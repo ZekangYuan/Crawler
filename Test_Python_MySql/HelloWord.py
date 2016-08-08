ab06f99127d06ee5fce9fc6a9092d101915adc6e
kangkang <1522847010@qq.com>
2016-08-08 14:34:32 +0800

    commit
    每次在关闭连接前进行提交

--- Test_Python_MySql/HelloWord.py
+++ Test_Python_MySql/HelloWord.py
@@ -30,20 +30,32 @@ print cursor
 #使用cursor.execute()执行select语句
 sql = "select * from user"
 cursor.execute(sql)
+conn.commit()
 print cursor.rowcount#获取结果集个数
 
-rs = cursor.fetchone()#获取当前第一条数据
-print rs
+rs1 = cursor.fetchone()#获取当前第一条数据
+print rs1
 
 
-rs = cursor.fetchmany(3)#获取多条数据
-print rs
+rs2 = cursor.fetchmany(3)#获取多条数据
+print rs2
 
-rs = cursor.fetchall()#获取剩余的数据
-print rs
+rs3 = cursor.fetchall()#获取剩余的数据
+print rs3
+
 
 #使用cursor.fetch*()获取并处理数据
 
-#4.关闭资源
+
+#((5L, u'name5'), (6L, u'name6'), (7L, u'name7'), (8L, u'name8'), (9L, u'name9'))
+#对结果使用二维遍历
+#由于结果是元组，使用元组遍历
+rs4 = cursor.fetchall()
+for row in rs4:
+    print "userid=%s, username=%s" % row#使用模板字符串进行打印
+
+
+
+#4.关闭资源  
 cursor.close()
 conn.close()
